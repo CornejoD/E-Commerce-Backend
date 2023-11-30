4aa60454e0e9b3f213d5a86cf638a5325c8769bf
Diego Cornejo <cornejodl13@icloud.com>
2023-11-30 10:22:14 -0600

    worked on columns

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,18 +3,45 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const categories = await Category.findAll({
+      include: [{ model: Product }],
+    });
+    res.status(200).json(categories);
+  }
+  catch (e) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const category = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+    res.status(200).json(category);
+  } catch (e) {
+    res
+      .status(500)
+      .json(e);
+    return;
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const newCategory = await Category.create(req.body);
+    res.status(200).json(newCategory);
+  }
+  catch (e) {
+    res.status(400).json(e);
+  }
 });
 
 router.put('/:id', (req, res) => {
